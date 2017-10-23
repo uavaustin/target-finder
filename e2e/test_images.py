0612bfa94a6e348fd63fc54a52e65074af71533d
Bradley Bridges <blbridges96@gmail.com>
2017-10-22 23:10:33 -0500

    Rename main e2e testing script and add a setup part to read in the images

---
+++ e2e/test_images.py
@@ -0,0 +1,10 @@
+from helpers.collect_images import collect_images
+
+images=None
+
+def setup_module():
+    images = collect_images()
+
+# TODO: Dynamically generate test cases for every image
+def test_all():
+    pass
