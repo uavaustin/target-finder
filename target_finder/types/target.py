2b9798e5bbfbb91bad37e6c6fe0924762d37328b
Paige Williams <paige.williams@utexas.edu>
2017-11-12 15:52:27 -0600

    Fixed Target class

--- target_finder/types/target.py
+++ target_finder/types/target.py
@@ -1,12 +1,37 @@
 import shapes
 import color
-import string
-import numpy as np
 class Target:
-    X = np.linspace(0,1,101)
-    Y = np.linspace(0,1,101)
-    ORIENTATION = list(range(0,360))
-    SHAPE = shapes
-    BACKGROUND_COLOR = color
-    ALPHANUMERIC = list(string.ascii_uppercase) + list(range(1,11))
-    ALPHANUMERIC_COLOR = color
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def shape(self):
+        return self._shape
+
+    @x.setter
+    def x(self, x):
+        if (x >= 0 and x <= 1):
+            self._x = x
+        else:
+            raise ValueError('0 <= x <= 1')
+
+    @y.setter
+    def y(self, y):
+        if y >= 0 and y <= 1:
+               self._y = y
+        else:
+            raise ValueError('0 <= y <= 1')
+
+    @shape.setter
+    def shape(self, shapes):
+        self._shape = shapes
