eed355fe42add23cc7e13d770d258e05fd6b59e7
Paige Williams <paige.williams@utexas.edu>
2017-11-16 18:29:13 -0600

    Added remaining variables to target class

--- target_finder/types/target.py
+++ target_finder/types/target.py
@@ -1,10 +1,11 @@
 import shapes
 import color
 class Target:
-    def __init__(self, x, y, shape):
+    def __init__(self, x, y, shape, background_color, alphanumeric_color = None, orientation = None, alphanumeric = None):
         self.x = x
         self.y = y
         self.shape = shape
+        self.background_color = color
 
     @property
     def x(self):
@@ -17,6 +18,22 @@ class Target:
     @property
     def shape(self):
         return self._shape
+
+    @property
+    def background_color(self):
+        return self._background_color
+
+    @property
+    def alphanumeric_color(self):
+        return self._alphanumeric_color
+
+    @property
+    def orientation(self):
+        return self._orientation
+
+    @property
+    def alphanumeric(self):
+        return self._alphanumeric
 
     @x.setter
     def x(self, x):
@@ -35,3 +52,27 @@ class Target:
     @shape.setter
     def shape(self, shapes):
         self._shape = shapes
+
+    @background_color.setter
+    def background_color(self, color):
+        self._background_color = color
+
+    @alphanumeric_color.setter
+    def alphanumeric_color(self, color):
+        return self._alphanumeric_color
+
+    @orientation.setter
+    def orientation(self, orientation):
+        if orientation >= 360:
+            orientation = orientation % 360
+            self._orientation = orientation
+        else:
+            self._orientation = orientation
+
+    @alphanumeric.setter
+    def alphanumeric(self, alphanumeric):
+        self._alphanumeric
+        if isalphnum(alphanumeric):
+            self._alphanumeric = alphanumeric
+        else:
+            raise ValueError('Must be an alphanumeric')
