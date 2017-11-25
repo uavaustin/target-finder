faa4c63b6744a87e5145b27d4154585b6be7300c
Bradley Bridges <blbridges96@gmail.com>
2017-11-25 00:08:15 -0600

    Added some testing for the x-position and y-position values for Targets

--- test/test_types.py
+++ test/test_types.py
@@ -1,5 +1,7 @@
 """Testing that the basic types in target_finder/types work."""
 
+import pytest
+
 from target_finder import Color, Shape, Target
 
 
@@ -9,3 +11,41 @@ def test_basic_target():
     assert t.x == 0.5
     assert t.y == 0.5
     assert t.shape == Shape.SQUARE
+
+
+def test_target_x_y_min_range():
+    with pytest.raises(ValueError):
+        t_1 = Target(-0.01, 0.5, Shape.SQUARE)
+
+    with pytest.raises(ValueError):
+        t_2 = Target(0.5, -0.01, Shape.SQUARE)
+        
+    t_3 = Target(0, 0, Shape.SQUARE)
+
+    with pytest.raises(ValueError):
+        t_3.x = -0.01
+
+    with pytest.raises(ValueError):
+        t_3.y = -0.01
+
+    assert t_3.x == 0
+    assert t_3.y == 0
+
+
+def test_target_x_y_max_range():
+    with pytest.raises(ValueError):
+        t_1 = Target(1.01, 1, Shape.SQUARE)
+
+    with pytest.raises(ValueError):
+        t_2 = Target(1, 1.01, Shape.SQUARE)
+        
+    t_3 = Target(1, 1, Shape.SQUARE)
+
+    with pytest.raises(ValueError):
+        t_3.x = 1.01
+
+    with pytest.raises(ValueError):
+        t_3.y = 1.01
+
+    assert t_3.x == 1
+    assert t_3.y == 1
