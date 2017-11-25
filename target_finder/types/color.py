dfce8610c9fe8813cbb227d321095d9d69dbd25e
Bradley Bridges <blbridges96@gmail.com>
2017-11-24 23:14:40 -0600

    Commented and fixed up the types directory.
    - Renamed `shapes.py` to `shape.py` to match the other files.
    
    - Added module, class, and function level docstrings.
    
    - Brought the files in accordance with PEP 8.
    
    - Removed the optional target attributes from the constructor. These will just
      have to be set later. (In practice, we won't need to set them all at the same
      time anyways.
    
    - Fixed some of the Target attribute checking, but not checking for the actual
      instances of the arguments.

--- target_finder/types/color.py
+++ target_finder/types/color.py
@@ -1,13 +1,25 @@
+"""Contains the Color enum for identifying colors in targets."""
+
 from enum import Enum
+
+
 class Color(Enum):
-    NONE = 0
-    WHITE = 1
-    BLACK = 2
-    GRAY = 3
-    RED = 4
-    BLUE = 5
-    GREEN = 6
+    """Contains colors for the AUVSI SUAS Interop Server.
+
+    These colors can be used for both the background color and the
+    alphanumeric color.
+
+    Note that Color.NONE can be used if a color cannot be identified.
+    """
+
+    NONE   = 0
+    WHITE  = 1
+    BLACK  = 2
+    GRAY   = 3
+    RED    = 4
+    BLUE   = 5
+    GREEN  = 6
     YELLOW = 7
     PURPLE = 8
-    BROWN = 9
+    BROWN  = 9
     ORANGE = 10
--- target_finder/types/target.py
+++ target_finder/types/target.py
@@ -1,39 +1,63 @@
-import shapes
-import color
-class Target:
-    def __init__(self, x, y, shape, background_color, alphanumeric_color = None, orientation = None, alphanumeric = None):
+"""Contains the Target class."""
+
+from math import fmod
+
+from color import Color
+from shape import Shape
+
+
+class Target(object):
+    """Represets a target found on an image.
+
+    This is intended to be built upon as the the target is being
+    classified.
+
+    Note that a target must have at least an x-position, y-position,
+    and a shape to be created.
+
+    A target should only be returned back by the library if at also
+    contains a background color as well.
+
+    Attributes:
+        x (float): The x position from left to right (0 <= x <= 1).
+        y (float): The y position from bottom to top (0 <= y <= 1).
+        orientation (float): The orientation of the target. An 
+            orientation of 0 means the target is not rotated, an
+            orientation of 90 means it the top of the target points 
+            to the right of the image (0 <= orientation < 360).
+        shape (Shape): The target shape.
+        background_color (Color): The target background color.
+        alphanumeric (str): The letter(s) and/or number(s) on the 
+            target. May consist of one or more of the characters 0-9,
+            A-Z, a-z. Typically, this will only be one capital
+            letter.
+        alphanumeric_color (Color): The target alphanumeric color.
+    """
+
+    def __init__(self, x, y, shape):
+        """Create a new Target object.
+
+        Args:
+            x (float): The x position from left to right
+                (0 <= x <= 1).
+            y (float): The y position from bottom to top
+                (0 <= y <= 1).
+            shape (Shape): The target shape.
+        """
+
         self.x = x
         self.y = y
         self.shape = shape
-        self.background_color = color
+
+        self.orientation = None
+        self.background_color = Color.NONE
+        self.alphanumeric = None
+        self.alphanumeric_color = Color.NONE
 
     @property
     def x(self):
+        """The x position from left to right."""
         return self._x
-
-    @property
-    def y(self):
-        return self._y
-
-    @property
-    def shape(self):
-        return self._shape
-
-    @property
-    def background_color(self):
-        return self._background_color
-
-    @property
-    def alphanumeric_color(self):
-        return self._alphanumeric_color
-
-    @property
-    def orientation(self):
-        return self._orientation
-
-    @property
-    def alphanumeric(self):
-        return self._alphanumeric
 
     @x.setter
     def x(self, x):
@@ -42,6 +66,11 @@ class Target:
         else:
             raise ValueError('0 <= x <= 1')
 
+    @property
+    def y(self):
+        """"The y position from bottom to top."""
+        return self._y
+
     @y.setter
     def y(self, y):
         if y >= 0 and y <= 1:
@@ -49,30 +78,57 @@ class Target:
         else:
             raise ValueError('0 <= y <= 1')
 
+    @property
+    def orientation(self):
+        """The orientation of the target."""
+        return self._orientation
+
+    @orientation.setter
+    def orientation(self, orientation):
+        if orientation is None:
+            self._orientation = None
+        else:
+            # Make sure the reading is in [0, 360). Note that fmod()
+            # is better with floats than the mod operator %.
+            self._orientation = fmod(fmod(orientation, 360) + 360, 360)
+
+    @property
+    def shape(self):
+        """The target shape."""
+        return self._shape
+
     @shape.setter
-    def shape(self, shapes):
-        self._shape = shapes
+    def shape(self, shape):
+        self._shape = shape
+
+    @property
+    def background_color(self):
+        """The target background color."""
+        return self._background_color
 
     @background_color.setter
     def background_color(self, color):
         self._background_color = color
 
-    @alphanumeric_color.setter
-    def alphanumeric_color(self, color):
-        return self._alphanumeric_color
-
-    @orientation.setter
-    def orientation(self, orientation):
-        if orientation >= 360:
-            orientation = orientation % 360
-            self._orientation = orientation
-        else:
-            self._orientation = orientation
+    @property
+    def alphanumeric(self):
+        """The letter(s) and/or number(s) on the target."""
+        return self._alphanumeric
 
     @alphanumeric.setter
     def alphanumeric(self, alphanumeric):
-        self._alphanumeric
-        if isalphnum(alphanumeric):
+        if alphanumeric is None:
+            self._alphanumeric = None
+        elif alphanumeric.isalnum():
             self._alphanumeric = alphanumeric
         else:
-            raise ValueError('Must be an alphanumeric')
+            raise ValueError('Must be alphanumeric')
+
+    @property
+    def alphanumeric_color(self):
+        """The target alphanumeric color."""
+        return self._alphanumeric_color
+
+    @alphanumeric_color.setter
+    def alphanumeric_color(self, color):
+        self._alphanumeric_color = color
