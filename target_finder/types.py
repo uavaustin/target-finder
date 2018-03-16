9c8e14070da559c2b9d70acd73988ba25103ba6a
Bradley Bridges <blbridges96@gmail.com>
2018-03-16 17:02:00 -0500

    Simplifying types into one file and dropping properties

---
+++ target_finder/types.py
@@ -0,0 +1,126 @@
+"""Contains basic storage types passed around in the library."""
+
+from enum import Enum, unique
+
+
+@unique
+class Color(Enum):
+    """Contains colors for the AUVSI SUAS Interop Server.
+
+    These colors can be used for both the background color and the
+    alphanumeric color.
+
+    Note that Color.NONE can be used if a color cannot be identified.
+    """
+
+    NONE   = 0
+    WHITE  = 1
+    BLACK  = 2
+    GRAY   = 3
+    RED    = 4
+    BLUE   = 5
+    GREEN  = 6
+    YELLOW = 7
+    PURPLE = 8
+    BROWN  = 9
+    ORANGE = 10
+
+
+@unique
+class Shape(Enum):
+    """Contains target shapes for the AUVSI SUAS Interop Server.
+
+    Note that Shape.NAS (not-a-shape) can be used if a shape cannot
+    be identified.
+    """
+
+    NAS            = 0
+    CIRCLE         = 1
+    SEMICIRCLE     = 2
+    QUARTER_CIRCLE = 3
+    TRIANGLE       = 4
+    SQUARE         = 5
+    RECTANGLE      = 6
+    TRAPEZOID      = 7
+    PENTAGON       = 8
+    HEXAGON        = 9
+    HEPTAGON       = 10
+    OCTAGON        = 11
+    STAR           = 12
+    CROSS          = 13
+
+
+class Blob(object):
+    """Represents a blob with a possible target.
+
+    This is for the middle stage before the target is identified.
+
+    This may be returned back by the library if only blobs are being
+    requested. This could be so that the logic is being seperated out
+    externally.
+
+    Attributes:
+        x (int): The x position of the top-left corner in pixels.
+        y (itn): The y position of the top-left corner in pixels.
+        width (int): The width of the blob in pixels.
+        height (int): The height of the blob in pixels.
+        image (PIL.Image): Image for the blob.
+    """
+
+    def __init__(self, x, y, width, height, image=None):
+        """Create a new Blob object."""
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+
+
+class Target(object):
+    """Represents a target found on an image.
+
+    This is intended to be built upon as the the target is being
+    classified.
+
+    Note that a target must have at least an x-position, y-position,
+    width, and height to be created.
+
+    A target should only be returned back by the library if at also
+    contains a background color as well.
+
+    Attributes:
+        x (int): The x position of the top-left corner in pixels.
+        y (int): The y position of the top-left corner in pixels.
+        width (int): The width of the blob in pixels.
+        height (int): The height of the blob in pixels.
+        orientation (float): The orientation of the target. An
+            orientation of 0 means the target is not rotated, an
+            orientation of 90 means it the top of the target points
+            to the right of the image (0 <= orientation < 360).
+        shape (Shape): The target shape.
+        background_color (Color): The target background color.
+        alphanumeric (str): The letter(s) and/or number(s) on the
+            target. May consist of one or more of the characters 0-9,
+            A-Z, a-z. Typically, this will only be one capital
+            letter.
+        alphanumeric_color (Color): The target alphanumeric color.
+        image (PIL.Image): Image showing the target.
+        confidence (float): The confidence that the target exists
+            (0 <= confidence <= 1).
+    """
+
+    def __init__(self, x, y, width, height, shape=Shape.NAS, orientation=0.0,
+                 background_color=Color.NONE, alphanumeric='',
+                 alphanumeric_color=Color.NONE, image=None, confidence=0.0):
+        """Create a new Target object."""
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.shape = shape
+        self.orientation = orientation
+        self.background_color = background_color
+        self.alphanumeric = alphanumeric
+        self.alphanumeric_color = alphanumeric_color
+        self.image = image
+        self.confidence = confidence
--- test/test_types.py
+++ test/test_types.py
@@ -7,159 +7,58 @@ from target_finder import Blob, Color, Shape, Target
 
 
 def test_basic_blob():
-    img = PIL.Image.new('1', (200, 300))
+    b = Blob(150, 250, 50, 100)
 
-    t = Blob(0.4, 0.6, img)
-
-    assert t.x == 0.4
-    assert t.y == 0.6
-    assert t.image == img
-
-
-def test_blob_x_y_min_range():
-    img = PIL.Image.new('1', (200, 300))
-
-    with pytest.raises(ValueError):
-        t_1 = Blob(-0.01, 0.5, img)
-
-    with pytest.raises(ValueError):
-        t_2 = Blob(0.5, -0.01, img)
-
-    t_3 = Blob(0, 0, img)
-
-    with pytest.raises(ValueError):
-        t_3.x = -0.01
-
-    with pytest.raises(ValueError):
-        t_3.y = -0.01
-
-    assert t_3.x == 0
-    assert t_3.y == 0
+    assert b.x == 150
+    assert b.y == 250
+    assert b.width == 50
+    assert b.height == 100
+    assert b.image == None
 
 
-def test_blob_x_y_max_range():
-    img = PIL.Image.new('1', (200, 300))
+def test_blob_with_image():
+    image = PIL.Image.new('1', (200, 300))
 
-    with pytest.raises(ValueError):
-        t_1 = Blob(1.01, 1, img)
+    b = Blob(100, 200, 20, 30, image=image)
 
-    with pytest.raises(ValueError):
-        t_2 = Blob(1, 1.01, img)
-
-    t_3 = Blob(1, 1, img)
-
-    with pytest.raises(ValueError):
-        t_3.x = 1.01
-
-    with pytest.raises(ValueError):
-        t_3.y = 1.01
-
-    assert t_3.x == 1
-    assert t_3.y == 1
+    assert b.x == 100
+    assert b.y == 200
+    assert b.width == 20
+    assert b.height == 30
+    assert b.image == image
 
 
 def test_basic_target():
-    t = Target(0.4, 0.6, Shape.SQUARE)
+    t = Target(2, 4, 5, 8)
 
-    assert t.x == 0.4
-    assert t.y == 0.6
+    assert t.x == 2
+    assert t.y == 4
+    assert t.width == 5
+    assert t.height == 8
+    assert t.shape == Shape.NAS
+    assert t.orientation == 0.0
+    assert t.background_color == Color.NONE
+    assert t.alphanumeric == ''
+    assert t.alphanumeric_color == Color.NONE
+    assert t.image == None
+    assert t.confidence == 0.0
+
+
+def test_target_with_optionals():
+    image = PIL.Image.new('1', (200, 300))
+
+    t = Target(3, 5, 7, 9, shape=Shape.SQUARE, orientation=74.3,
+               background_color=Color.GREEN, alphanumeric='A',
+               alphanumeric_color=Color.WHITE, image=image, confidence=0.97)
+
+    assert t.x == 3
+    assert t.y == 5
+    assert t.width == 7
+    assert t.height == 9
     assert t.shape == Shape.SQUARE
-
-
-def test_target_x_y_min_range():
-    with pytest.raises(ValueError):
-        t_1 = Target(-0.01, 0.5, Shape.SQUARE)
-
-    with pytest.raises(ValueError):
-        t_2 = Target(0.5, -0.01, Shape.SQUARE)
-
-    t_3 = Target(0, 0, Shape.SQUARE)
-
-    with pytest.raises(ValueError):
-        t_3.x = -0.01
-
-    with pytest.raises(ValueError):
-        t_3.y = -0.01
-
-    assert t_3.x == 0
-    assert t_3.y == 0
-
-
-def test_target_x_y_max_range():
-    with pytest.raises(ValueError):
-        t_1 = Target(1.01, 1, Shape.SQUARE)
-
-    with pytest.raises(ValueError):
-        t_2 = Target(1, 1.01, Shape.SQUARE)
-
-    t_3 = Target(1, 1, Shape.SQUARE)
-
-    with pytest.raises(ValueError):
-        t_3.x = 1.01
-
-    with pytest.raises(ValueError):
-        t_3.y = 1.01
-
-    assert t_3.x == 1
-    assert t_3.y == 1
-
-
-def test_target_orientation_range():
-    t = Target(0.5, 0.5, Shape.CIRCLE)
-
-    testing_values = [
-        [1, 1],
-        [359, 359],
-        [0, 0],
-        [360, 0],
-        [-1, 359],
-        [-1000, 80],
-        [1000, 280],
-        [123.456789, 123.456789]
-    ]
-
-    # Looping through them each and allowing for an absolute error of
-    # 1e-10.
-    for pair in testing_values:
-        actual = pair[0]
-        expected = pair[1]
-
-        t.orientation = actual
-
-        assert abs(expected - t.orientation) < 1e-10
-
-
-def test_target_alphanumeric():
-    t = Target(0.5, 0.5, Shape.OCTAGON)
-
-    valid = ['A', 'Z', 'a', 'z', '0', '9', 'Word', '123', 'h4llo']
-    invalid = [' ', 'Invalid Input', '#', '$$$', '*']
-
-    for string in valid:
-        t.alphanumeric = string
-        assert t.alphanumeric == string
-
-    for string in invalid:
-        with pytest.raises(ValueError):
-            t.alphanumeric = string
-
-
-def test_target_confidence_range():
-    t = Target(0.5, 0.5, Shape.HEXAGON)
-
-    t.confidence = 0.5
-    assert t.confidence == 0.5
-
-    t.confidence = 0
-    assert t.confidence == 0
-
-    t.confidence = 1
-    assert t.confidence == 1
-
-    with pytest.raises(ValueError):
-        t.confidence = -0.01
-
-    with pytest.raises(ValueError):
-        t.confidence = 1.01
-
-    assert t.confidence == 1
+    assert t.orientation == 74.3
+    assert t.background_color == Color.GREEN
+    assert t.alphanumeric == 'A'
+    assert t.alphanumeric_color == Color.WHITE
+    assert t.image == image
+    assert t.confidence == 0.97
