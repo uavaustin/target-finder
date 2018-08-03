6ea3601e8d7f77d005c8949994389fc781c7c019
Bradley Bridges <blbridges96@gmail.com>
2018-08-03 17:53:09 -0500

    Remove model and requirements file from create-release.sh

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -2,13 +2,6 @@
 
 cd $(dirname "$0")
 
-graph_file="../target_finder/data/graph.pb"
-labels_file="../target_finder/data/labels.txt"
-
-# Check that the model files exist.
-[ -f "$graph_file" ] || (>&2 echo "Missing graph.pb" && exit 1)
-[ -f "$labels_file" ] || (>&2 echo "Missing labels.txt" && exit 1)
-
 # Find the version number to release.
 version=$(grep -o -e "'.*'" "../target_finder/version.py" | tr -d "'")
 
@@ -28,12 +21,9 @@ find "../target_finder/" -name "*.py" -exec cp '{}' \
 
 # Copy over the graph and labels.
 mkdir -p "$tf_stage_dir""/target_finder/data"
-cp "$graph_file" "$tf_stage_dir""/target_finder/data/"
-cp "$labels_file" "$tf_stage_dir""/target_finder/data/"
 
 # Copy over configuration and informational files.
-cp ../README.md ../LICENSE ../CHANGELOG.md ../MANIFEST.in ../requirements.txt \
-  ../setup.py "$tf_stage_dir"
+cp ../README.md ../LICENSE ../CHANGELOG.md ../setup.py "$tf_stage_dir"
 
 # Compress the directory.
 echo "Creating archive"
