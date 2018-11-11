1f0ec416de44b8210ca99bff877933e537a316b2
Shrivu Shankar <shrivu1122@gmail.com>
2018-11-11 15:41:04 -0600

    Renamed `quarter_circle`
    I had to indent it since the max line length was reached and since only one indented looked weird, I indented all of them.

--- test/test_prepro.py
+++ test/test_prepro.py
@@ -7,17 +7,28 @@ from target_finder.preprocessing import find_blobs
 from target_finder.types import Blob
 
 TESTS = [
-    ('quartcircle.jpg', Blob(x=11, y=13, width=60, height=59, has_mask=True)),
-    ('semicircle.jpg', Blob(x=10, y=28, width=60, height=31, has_mask=True)),
-    ('circle.jpg', Blob(x=10, y=10, width=61, height=62, has_mask=True)),
-    ('triangle.jpg', Blob(x=12, y=15, width=58, height=52, has_mask=True)),
-    ('trapezoid.jpg', Blob(x=11, y=19, width=57, height=45, has_mask=True)),
-    ('square.jpg', Blob(x=17, y=18, width=47, height=47, has_mask=True)),
-    ('star.jpg', Blob(x=12, y=13, width=56, height=51, has_mask=True)),
-    ('pentagon.jpg', Blob(x=13, y=13, width=56, height=54, has_mask=True)),
-    ('hexagon.jpg', Blob(x=10, y=14, width=60, height=54, has_mask=True)),
-    ('heptagon.jpg', Blob(x=13, y=14, width=56, height=54, has_mask=True)),
-    ('octagon.jpg', Blob(x=10, y=12, width=59, height=58, has_mask=True))
+    ('quarter_circle.jpg',
+        Blob(x=11, y=13, width=60, height=59, has_mask=True)),
+    ('semicircle.jpg',
+        Blob(x=10, y=28, width=60, height=31, has_mask=True)),
+    ('circle.jpg',
+        Blob(x=10, y=10, width=61, height=62, has_mask=True)),
+    ('triangle.jpg',
+        Blob(x=12, y=15, width=58, height=52, has_mask=True)),
+    ('trapezoid.jpg',
+        Blob(x=11, y=19, width=57, height=45, has_mask=True)),
+    ('square.jpg',
+        Blob(x=17, y=18, width=47, height=47, has_mask=True)),
+    ('star.jpg',
+        Blob(x=12, y=13, width=56, height=51, has_mask=True)),
+    ('pentagon.jpg',
+        Blob(x=13, y=13, width=56, height=54, has_mask=True)),
+    ('hexagon.jpg',
+        Blob(x=10, y=14, width=60, height=54, has_mask=True)),
+    ('heptagon.jpg',
+        Blob(x=13, y=14, width=56, height=54, has_mask=True)),
+    ('octagon.jpg',
+        Blob(x=10, y=12, width=59, height=58, has_mask=True))
 ]
 
 
