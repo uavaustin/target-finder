e36355e61a494bc93c7f37950eaa0e9f0fdd5c08
Shrivu Shankar <shrivu1122@gmail.com>
2018-11-13 20:10:40 -0600

    Auto Exclude Background
    I noticed that on the new testcases this was breaking and it turns out there was a bug with small input imgs not filtering out the background.

--- target_finder/preprocessing.py
+++ target_finder/preprocessing.py
@@ -39,7 +39,7 @@ def find_blobs(image, min_width=20, max_width=100, limit=100, padding=20):
 
     largest_label = -1  # the largest label (might not be > max_width)
     largest_size = -1
-    large_labels = []  # all blobs to big to be a shape (> max_width)
+    large_labels = [labels[0, 0]]  # all blobs to big to be a shape (> max)
 
     num_parts = np.max(labels) + 1
     for label in range(0, num_parts):
